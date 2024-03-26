d7bbc62ab211a4ae916abdbe74839186700a6aac
wesselvtoorn <114135813+wesselvtoorn@users.noreply.github.com>
2024-03-26 14:39:40 +0100

    calibratie variables zijn in deze gevonden

--- test_code_gyroscoop/src/main.cpp
+++ test_code_gyroscoop/src/main.cpp
@@ -48,18 +48,26 @@ void loop() {
 	// Serial.println(" m/s^2");
 
 	// rotation
-	x_as += round((g.gyro.x + 0.12)*10);
-	y_as += round((g.gyro.y + 0.025)*10);
-	z_as += round((g.gyro.z + 0.055)*10);
+	// x: + 0.12 y: + 0.25 z: + 0.055
+
+	// rotation donker
+	// x: + 0.064 y: - 0.01 z: + 0.0044
+	x_as += round((g.gyro.x + 0.12)*100)*2.195121951219512;
+	y_as += round((g.gyro.y + 0.025)*100)*2.195121951219512;
+	z_as += round((g.gyro.z + 0.055)*100)*2.195121951219512;
+
+	// x_as = round(x_as);
+	// y_as = round(y_as);
+	// z_as = round(z_as);
 
 
 	Serial.print("Rotation X: ");
-	Serial.print(x_as);
+	Serial.print(round(x_as/1000));
 	Serial.print(", Y: ");
-	Serial.print(y_as);
+	Serial.print(round(y_as/1000));
 	Serial.print(", Z: ");
-	Serial.print(z_as);
-	Serial.println(" rad");
+	Serial.print(round(z_as/1000));
+	Serial.println(" grad");
 
 	// temperature
 	// Serial.print("Temperature: ");
@@ -68,3 +76,64 @@ void loop() {
 
 	delay(0.1);
 }
+
+// #include <Adafruit_MPU6050.h>
+// #include <Adafruit_Sensor.h>
+// #include <Wire.h>
+// #include <Arduino.h>
+
+// Adafruit_MPU6050 mpu;
+
+// void setup(void) {
+// 	Serial.begin(115200);
+
+// 	// Try to initialize!
+// 	if (!mpu.begin()) {
+// 		Serial.println("Failed to find MPU6050 chip");
+// 		while (1) {
+// 		  delay(10);
+// 		}
+// 	}
+// 	Serial.println("MPU6050 Found!");
+
+// 	// set accelerometer range to +-8G
+// 	mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+
+// 	// set gyro range to +- 500 deg/s
+// 	mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+
+// 	// set filter bandwidth to 21 Hz
+// 	mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+// 	delay(100);
+// }
+
+// void loop() {
+// 	/* Get new sensor events with the readings */
+// 	sensors_event_t a, g, temp;
+// 	mpu.getEvent(&a, &g, &temp);
+
+// 	/* Print out the values */
+// 	Serial.print("Acceleration X: ");
+// 	Serial.print(a.acceleration.x);
+// 	Serial.print(", Y: ");
+// 	Serial.print(a.acceleration.y);
+// 	Serial.print(", Z: ");
+// 	Serial.print(a.acceleration.z);
+// 	Serial.println(" m/s^2");
+
+// 	Serial.print("Rotation X: ");
+// 	Serial.print(g.gyro.x);
+// 	Serial.print(", Y: ");
+// 	Serial.print(g.gyro.y);
+// 	Serial.print(", Z: ");
+// 	Serial.print(g.gyro.z);
+// 	Serial.println(" rad/s");
+
+// 	Serial.print("Temperature: ");
+// 	Serial.print(temp.temperature);
+// 	Serial.println(" degC");
+
+// 	Serial.println("");
+// 	delay(500);
+// }
